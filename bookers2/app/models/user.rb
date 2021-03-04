fd2c7add934128a87d6179093d5c87f8e688ed07
potetakoyaki <potetakoyaki@gmail.com>
2021-03-04 05:50:37 +0000

    [finish]応用課題３

--- bookers2/app/models/user.rb
+++ bookers2/app/models/user.rb
@@ -3,8 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-  has_many :books
+  has_many :favorites, dependent: :destroy
+  has_many :books, dependent: :destroy
+  has_many :book_comments, dependent: :destroy
+  
   attachment :profile_image, destroy: false
 
   validates :name, length: {maximum: 20, minimum: 2}, uniqueness: true
--- bookers2/config/routes.rb
+++ bookers2/config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+  get 'favorites/create'
+  get 'favorites/destroy'
   root 'homes#top'
   get 'home/about' => 'homes#about'
   devise_for :users
-  resources :books
   resources :users,only: [:show,:index,:edit,:update]
+  resources :books do
+    resource :favorites,only: [:create, :destroy]
+    resources :book_comments,only: [:create, :destroy]
+  end
 end
