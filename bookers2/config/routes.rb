dfdd2feda9f555fffb0f040a7f22ccd26e3f5241
potetakoyaki <potetakoyaki@gmail.com>
2021-03-05 06:07:02 +0000

    [start]応用課題４

--- bookers2/config/routes.rb
+++ bookers2/config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get 'home/about' => 'homes#about'
   devise_for :users
   resources :users,only: [:show,:index,:edit,:update]
+  resources :relationships, only: [:create, :destroy]
   resources :books do
     resource :favorites,only: [:create, :destroy]
     resources :book_comments,only: [:create, :destroy]
