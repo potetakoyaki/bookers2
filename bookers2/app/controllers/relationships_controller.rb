4ba630710c4d821f240b3f8c1dfe088834a5d795
potetakoyaki <potetakoyaki@gmail.com>
2021-03-06 12:36:57 +0000

    [finish]応用課題４

--- bookers2/app/controllers/relationships_controller.rb
+++ bookers2/app/controllers/relationships_controller.rb
@@ -1,2 +1,22 @@
 class RelationshipsController < ApplicationController
+
+  def follow
+    current_user.follow(params[:id])
+    redirect_back(fallback_location: root_path)
+  end
+
+  def unfollow
+    current_user.unfollow(params[:id])
+    redirect_back(fallback_location: root_path)
+  end
+
+  def follower
+    @user = User.find(params[:id])
+    @users = @user.following_user
+  end
+
+  def followed
+    @user = User.find(params[:id])
+    @users = @user.follower_user
+  end
 end
--- bookers2/config/routes.rb
+++ bookers2/config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
+  post 'follow/:id' => 'relationships#follow', as: 'follow'
+  post 'unfollow/:id' => 'relationships#unfollow', as: 'unfollow'
+  get 'follower/:id' => 'relationships#follower', as: 'follower'
+  get 'followed/:id' => 'relationships#followed', as: 'followed'
   get 'favorites/create'
   get 'favorites/destroy'
   root 'homes#top'
   get 'home/about' => 'homes#about'
   devise_for :users
   resources :users,only: [:show,:index,:edit,:update]
-  resources :relationships, only: [:create, :destroy]
   resources :books do
     resource :favorites,only: [:create, :destroy]
     resources :book_comments,only: [:create, :destroy]
